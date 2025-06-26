d746b323c36f97d85cfc9f37251b9ec1495f91ea
Jasmina <jasminethapa330@gmail.com>
2025-06-26 11:33:45 +0545

    Add project files.

---
+++ DrinksInfo.Jasmine330/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DrinksInfo.Jasmine330;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        UserInput userInput = new();
+        userInput.GetCategoriesInput();
+    }
+}
---
+++ DrinksInfo.Jasmine330/Validator.cs
@@ -0,0 +1,41 @@
+﻿namespace DrinksInfo.Jasmine330
+{
+    class Validator
+    {
+        internal static bool IsIdValid(string stringInput)
+        {
+            if (String.IsNullOrEmpty(stringInput))
+            {
+                return false;
+            }
+
+            foreach (char c in stringInput)
+            {
+                if (!Char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        internal static bool IsStringValid(string stringInput)
+        {
+            if (String.IsNullOrEmpty(stringInput))
+            {
+                return false;
+            }
+
+            foreach(char c in stringInput)
+            {
+                if (!char.IsLetter(c) && c != '/' && c!= ' ')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
